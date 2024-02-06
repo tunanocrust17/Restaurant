d02ea821acaeb874c1cb591ce9ebc18f0a620f1a
ewhuet <ewhuet@gmail.com>
2024-02-06 12:29:58 +0200

    removed unused code that was testing functionality

--- src/home.js
+++ src/home.js
@@ -1,9 +1,5 @@
 
-// import './style.css';
-
 export default function homePage() {
-
-    const content = document.getElementById('content');
 
     //Create the div for the content of the home page
     const el = document.createElement('div');
@@ -19,12 +15,7 @@ export default function homePage() {
     descriptionH2.innerHTML = "Welcome to The Giggling Grouper, where the laughter is as abundant as the fish in our tanks! Dive into a sea of silliness as you reel in the freshest catches with a side of laughter. Our fishy friends aren't the only ones with a sense of humor – our staff will have you hooked with their fin-tastic jokes and bubbly personalities."
     descriptionDiv.appendChild(descriptionH2)
     el.appendChild(descriptionDiv);
-    // content.appendChild(el);
 
     return el;
     
 }
-
-// export default function printMe() {
-//   console.log('I get called from print.js!');
-// }
