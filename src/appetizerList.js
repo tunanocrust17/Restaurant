0b1289f3aa5815414fad87314baf088df3ba5781
Eric Huet <ewhuet@gmail.com>
2024-02-12 20:16:42 +0200

    created function that holds and object of all the appetizer items

---
+++ src/appetizerList.js
@@ -0,0 +1,10 @@
+export default function appetizerList() {
+
+    let appetizerItems = [{
+        name: "Chuckling Crab Cakes",
+        description: "Fluffy crab cakes made with tender lump crab meat, seasoned with a hint of Old Bay and zesty Dijon mustard. Pan-fried to golden perfection and served with a dollop of creamy tartar sauce.",
+        price: "$6.66"
+    }]
+
+    return appetizerItems;
+}
